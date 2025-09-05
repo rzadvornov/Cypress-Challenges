a18740c7c55e81931b660b25c84191e229a558ce
rzadvornov <roman.zadvornov1-ext@leaseplan.com>
2025-09-05 23:06:37 +0300

    Configuring parallel run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,14 @@ set -e
 
 echo "Running Cypress tests with Cypress Cloud parallelization"
 echo "CI Build ID: $CI_BUILD_ID"
+echo "Machine Index: $SPLIT_INDEX"
 
-# Run cypress with record flag - Cypress Cloud will handle parallelization
-npx cypress run --record --key "$CYPRESS_RECORD_KEY" --ci-build-id "$CI_BUILD_ID"
+# Run cypress with record, parallel, and group flags
+npx cypress run \
+  --record \
+  --parallel \
+  --group "Machine ${SPLIT_INDEX:-0}" \
+  --key "$CYPRESS_RECORD_KEY" \
+  --ci-build-id "$CI_BUILD_ID"
 
 exit $?
