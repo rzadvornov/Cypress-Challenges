f87e4af071e73fda2452f650dc21a648d1272baa
rzadvornov <roman.zadvornov1-ext@leaseplan.com>
2025-09-05 18:12:26 +0300

    Updated docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,15 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "Running Cypress tests in parallel on CI using cypress-split"
+echo "Running Cypress tests in parallel with cypress-split"
 
 # The npm exec command is a more robust way to run package executables.
-npm exec cypress-split run \
-  -- \
-  --ci \
-  --total "${SPLIT_TOTAL_MACHINES}" \
-  --group "${SPLIT_MACHINE_INDEX}" \
+# This script explicitly uses the environment variables to split the tests.
+npx cypress-split run \
+  --ci-build-id ${GITHUB_RUN_ID} \
+  --parallel \
+  --record false \
+  --group "Machine ${SPLIT_MACHINE_INDEX}" \
   "$@"
 
 # Pass the exit code of the Cypress command.
