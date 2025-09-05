cd4c0fa43ea4572152344dff62202cffc256623e
rzadvornov <roman.zadvornov1-ext@leaseplan.com>
2025-09-05 23:02:42 +0300

    Configuring parallel run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "Running Cypress tests in parallel with Cypress Cloud"
-echo "Environment variables:"
-echo "CI_BUILD_ID=$CI_BUILD_ID"
-echo "SPLIT_INDEX=$SPLIT_INDEX"
+echo "Running Cypress tests with Cypress Cloud parallelization"
+echo "CI Build ID: $CI_BUILD_ID"
 
-# Run cypress with record flag for Cypress Cloud parallelization
-npx cypress run \
-  --record \
-  --key "$CYPRESS_RECORD_KEY" \
-  --parallel \
-  --group "Machine ${SPLIT_INDEX}" \
-  --ci-build-id "$CI_BUILD_ID" \
-  "$@"
+# Run cypress with record flag - Cypress Cloud will handle parallelization
+npx cypress run --record --key "$CYPRESS_RECORD_KEY" --ci-build-id "$CI_BUILD_ID"
 
-# Pass the exit code of the Cypress command.
 exit $?
